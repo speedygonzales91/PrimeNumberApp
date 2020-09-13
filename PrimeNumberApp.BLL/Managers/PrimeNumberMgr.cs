b1dc1df6186faea10085a342223ec5480939ad5c
Molnar Zsolt <mozb17@gmail.com>
2020-09-13 20:44:46 +0200

    Add PrimeNumberMgr class. Add the necessary calculation functions.
    Add PrimeNumberMgr class. Add the necessary calculation functions.

---
+++ PrimeNumberApp.BLL/Managers/PrimeNumberMgr.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeNumberApp.BLL.Managers
+{
+    /// <summary>
+    /// Manager class for prime number calculations
+    /// </summary>
+    public class PrimeNumberMgr
+    {
+        /// <summary>
+        /// Check number is prime or not
+        /// </summary>
+        /// <param name="number">number to check</param>
+        /// <returns></returns>
+        public bool IsPrime(int number)
+        {
+            // Base case 
+            if (number <= 1)
+                return false;
+
+            // Check from 2 to number-1 
+            for (var i = 2; i < number; i++)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Determines the next smallest prime number after a given number. If parameter less than 1 returns 2.
+        /// </summary>
+        /// <param name="number">number to check the smallest prime number from</param>
+        /// <returns></returns>
+        public int NextPrimeAfterGivenNumber(int number)
+        {
+            // Base case
+            if (number <= 1)
+                return 2;
+
+            var primeNumber = number;
+            var isFound = false;
+
+            // Lopp till found the smallest prime
+            while (!isFound)
+            {
+                primeNumber++;
+
+                if (IsPrime(primeNumber))
+                    isFound = true;
+            }
+
+            return primeNumber;
+        }
+    }
+}
