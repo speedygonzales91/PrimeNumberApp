5c0cc1912a3c5671400290ccbc9b37b798567803
Molnar Zsolt <mozb17@gmail.com>
2020-09-13 22:30:15 +0200

    Remove unnecessary files. Set Up Controller. Add launchSettings.json
    Remove unnecessary files. Set Up Controller. Add launchSettings.json

---
+++ PrimeNumberApp.API/Controllers/PrimeNumberController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeNumberApp.BLL.Interfaces.Managers;
+
+namespace PrimeNumberApp.API.Controllers
+{
+    
+    [Route("api/primeNumber")]
+    [ApiController]
+    public class PrimeNumberController : ControllerBase
+    {
+        private readonly IPrimeNumberMgr _primeNumberMgr;
+
+        public PrimeNumberController(IPrimeNumberMgr primeNumberMgr)
+        {
+            _primeNumberMgr = primeNumberMgr;
+        }
+
+        [HttpGet]
+        public ActionResult<bool> Index()
+        {
+            return Ok();
+        }
+
+        [Route("isPrime/{number}")]
+        [HttpGet]
+        public ActionResult<bool> IsPrime(int number)
+        { 
+            return Ok(_primeNumberMgr.IsPrime(number));
+        }
+
+        [Route("nextPrime/{number}")]
+        [HttpGet]
+        public ActionResult<int> NextPrime(int number)
+        {
+            return Ok(_primeNumberMgr.NextPrimeAfterGivenNumber(number));
+        }
+    }
+}
--- PrimeNumberApp.API/Startup.cs
+++ PrimeNumberApp.API/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PrimeNumberApp.BLL.Interfaces.Managers;
+using PrimeNumberApp.BLL.Managers;
 
 namespace PrimeNumberApp.API
 {
@@ -26,6 +28,10 @@ namespace PrimeNumberApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            #region Manager registration
+            services.AddScoped<IPrimeNumberMgr, PrimeNumberMgr>();
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
