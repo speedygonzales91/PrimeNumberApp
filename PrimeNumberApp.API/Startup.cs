e1b23259b54667cecf17e1165a139b9698698daf
Molnar Zsolt <mozb17@gmail.com>
2020-09-14 09:33:19 +0200

    Add swagger endpoint
    Add swagger endpoint

--- PrimeNumberApp.API/Startup.cs
+++ PrimeNumberApp.API/Startup.cs
@@ -29,9 +29,13 @@ namespace PrimeNumberApp.API
         {
             services.AddControllers();
 
-            #region Manager registration
+            #region Managers registration
             services.AddScoped<IPrimeNumberMgr, PrimeNumberMgr>();
             #endregion
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +47,17 @@ namespace PrimeNumberApp.API
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            
 
             app.UseRouting();
 
